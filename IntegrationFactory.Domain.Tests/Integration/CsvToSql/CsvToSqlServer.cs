72ed2e81648a76af19d73d9a0ce3ff9fd948053b
silverio <lucas.silverio@managerbs.com>
2020-09-28 23:34:58 -0300

    integration-test csv-to-sqlserver

---
+++ IntegrationFactory.Domain.Tests/Integration/CsvToSql/CsvToSqlServer.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using IntegrationFactory.Domain.DataSet;
+using IntegrationFactory.Domain.DataSet.PlainText;
+using IntegrationFactory.Domain.DataSet.SqlServer;
+using IntegrationFactory.Domain.PipeLine;
+using IntegrationFactory.Domain.Tests.SeedWork;
+using Xunit;
+
+namespace IntegrationFactory.Domain.Tests
+{
+    public class CsvToSqlServer
+    {
+
+        [Fact]
+        public void CsvToSqlServerExecute()
+        {
+
+            var pipeLine = new PipeLineContext<Region>()
+                  .SetOrigin(new PlainTextOrigin<Region>()
+                                .SetPath(CSV.Path)
+                                .SetSeparator(';')
+                                .SetMapping(x => new Region()
+                                {
+                                    Id = Convert.ToInt32(x[0]),
+                                    Initials = x[1],
+                                    Name = x[2]
+                                }))
+                  .SetDestiny(new SqlServerDestiny()
+                                .SetConnection(Connections.LocalDataBase)
+                                .SetTable("Regiao")
+                                .SetMapping(
+                                    new List<Map>(){
+                                    new Map("Id", "Identidade"),
+                                    new Map("Initials", "Sigla"),
+                                    new Map("Name", "NomeDaRegiao")
+                                }))
+                  .Extract()
+                  .Transform<Region>()
+                  .Load();
+
+            Assert.Equal("3 itens incluídos", pipeLine.Destiny.Result.Message);
+
+        }
+    }
+}
